a152737cf358295fb3c737d9964d741466afb5c2
Mike <Walker>
2019-10-29 08:18:22 +0000

    Handle postcode errors.

--- src/main/java/training/busboard/postcodes/PostcodeApiClient.java
+++ src/main/java/training/busboard/postcodes/PostcodeApiClient.java
@@ -3,6 +3,7 @@ package training.busboard.postcodes;
 import training.busboard.postcodes.models.PostcodeData;
 import training.busboard.postcodes.models.PostcodeResult;
 
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
@@ -15,16 +16,21 @@ public class PostcodeApiClient {
     }
 
     public PostcodeData getPostcodeDate(String postcode) {
-        PostcodeResult result = client
-                .target("http://api.postcodes.io/")
-                .path("postcodes/" + postcode)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get(PostcodeResult.class);
+        try {
+            PostcodeResult result = client
+                    .target("http://api.postcodes.io/")
+                    .path("postcodes/" + postcode)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(PostcodeResult.class);
 
-        if (result.getStatus() == 200) {
-            return result.getResult();
+            if (result.getStatus() == 200) {
+                return result.getResult();
+            }
+
+            return null;
+        } catch (NotFoundException e) {
+            System.out.printf("\033[33mSorry - we weren't able to find any data for the postcode %s. Please try again.\033[0m\n", postcode);
+            return null;
         }
-
-        return null;
     }
 }
